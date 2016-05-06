7a2284d99d5b4a0cfd8c64a8bec264526583e218
Michał Fierek <mfierek@p109-14.labpk.inf.ug.edu.pl>
2016-05-06 10:55:07 +0200

    edit

--- lab07/zad01.c
+++ lab07/zad01.c
@@ -4,7 +4,7 @@
 
 int main (int argc, char *argv[])
 {
-  for ( int i = --argc; i > 0; --i) {
+  for (int i = --argc; i > 0; --i) {
     printf("%s", argv[i]);
     if (i != 1 ) printf(" ");
     else printf ("\n");
