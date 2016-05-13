db4d70d0032f67d2a694145ade4008787ff8b32c
Michał <mfierek2@users.noreply.github.com>
2016-05-13 11:11:06 +0200

    Update zad02.php

--- lab09/zad02.php
+++ lab09/zad02.php
@@ -1,46 +1,48 @@
 <div class = "input">
-				<h3>Nowa osoba:</h3>
-				  <form>
-				  <input type="text" name="nowy">
-				  <?php
-					error_reporting(0);
-				  if($_REQUEST['ile']!=NULL)
-				  {
-					for($i=0;$i<=$_REQUEST['ile'];$i++)
+	<h3>Nowa osoba:</h3>
+	<form>
+		<input type="text" name="nowy">
+		<?php
+			error_reporting(0);
+			if($_REQUEST['ile']!=NULL)
+			{
+				for($i=0;$i<=$_REQUEST['ile'];$i++)
+				{
+					if($i< $_REQUEST['ile'] )
 					{
-						if($i< $_REQUEST['ile'] )
-						{
-							$a=$_REQUEST['imie'.$i];
-							echo '<input type="hidden" name="imie'.$i.'" value="'.$a.'" />'."\n";
-						}else
-						{
-							echo '<input type="hidden" name="imie'.$i.'" value="'.$_REQUEST['nowy'].'" />'."\n";
-						}
+						$a=$_REQUEST['imie'.$i];
+						echo '<input type="hidden" name="imie'.$i.'" value="'.$a.'" />'."\n";
 					}
-				 }
-				?>
-			</div>
-			<input type ="hidden" name="ile" value="<?php if($_REQUEST['ile']==NULL){ echo "0";}
-			if($_REQUEST['ile']!=NULL){ $a= $_REQUEST['ile']; echo $a+1;}?>">
-</form>
-<div class = "list">
-<?php
-	error_reporting(0);
-	if ($_REQUEST['ile'] != NULL)
-	{
-		for ($i = 0; $i <= $_REQUEST['ile']; $i++)
-		{
-			if($i< $_REQUEST['ile'] )
-			{
-				$a= $_REQUEST['imie'.$i] ;
-				echo $i+1 .". ".$a."<br>";
+					else
+					{
+						echo '<input type="hidden" name="imie'.$i.'" value="'.$_REQUEST['nowy'].'" />'."\n";
+					}
+				}
 			}
-			else
+		?>
+	<input type ="hidden" name="ile" value="<?php if($_REQUEST['ile']==NULL){ echo "0";}
+		      if($_REQUEST['ile']!=NULL){ $a= $_REQUEST['ile']; echo $a+1;}?>">
+	</form>
+</div>
+
+<div class = "list">
+	<?php
+		error_reporting(0);
+		if ($_REQUEST['ile'] != NULL)
+		{	
+			for ($i = 0; $i <= $_REQUEST['ile']; $i++)
 			{
-				echo $i+1 .". ".$_REQUEST['nowy']."<br>" ;
+				if($i< $_REQUEST['ile'] )
+				{
+					$a= $_REQUEST['imie'.$i] ;
+					echo $i+1 .". ".$a."<br>";
+				}
+				else
+				{
+					echo $i+1 .". ".$_REQUEST['nowy']."<br>" ;
+				}
 			}
 		}
-}
-?>
-		</div>
-	</div>
+	?>
+</div>
+
