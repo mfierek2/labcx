de1675a87b473aa93e5fcd83a4ee052b9756ebcd
Michał <mfierek2@users.noreply.github.com>
2016-05-13 11:14:24 +0200

    Update zad03.c

--- lab08/zad03.c
+++ lab08/zad03.c
@@ -1,9 +1,14 @@
+// Program potrzebuje czytania wielu danych rzeczywistych i całkowitych z klawiatury; 
+// za każdym razem powinien napisać na monitorze, jakiej zmiennej potrzebuje i poczekać,
+// aż użytkownik wpisze odpowiednią daną. Napisać makrodefinicję CZYT, która to realizuje.
+// Na przykład wywołanie makra CZYT(a,<lf) powinno spowodowac wydruk
+// Podaj wartosc a:
+// a następnie wczytanie liczby rzeczywistej do zmiennej a.
+// Oczywiście jeśli drugim argumentem makra jest i, to chodzi o wczytanie liczby całkowitej.
 #include <stdio.h>
-
 #define CZYT(liczba, typ)\
  printf("Podaj wartosc "#liczba": ");\
  scanf("%"#typ, &liczba);
-
 int main ()
 {
   int x;
