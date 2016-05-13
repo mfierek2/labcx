8b1a13f1b69cc70f33ca4cf47f29fbee9a898f5a
Michał <mfierek2@users.noreply.github.com>
2016-05-13 11:01:41 +0200

    Update zad03.php

--- lab09/zad03.php
+++ lab09/zad03.php
@@ -8,14 +8,14 @@
 				}
 
 				$f = fopen($counter_name,"r");
-				$counterVal = fread($f, filesize($counter_name));
+				$counter_val = fread($f, filesize($counter_name));
 				fclose($f);
 
-				if(!isset($_SESSION['hasVisited'])){
-				  $_SESSION['hasVisited']="yes";
+				if(!isset($_SESSION['has_visited'])){
+				  $_SESSION['has_visited']="yes";
 				  $counterVal++;
 				  $f = fopen($counter_name, "w");
-				  fwrite($f, $counterVal);
+				  fwrite($f, $counter_val);
 				  fclose($f);
 				}
 				echo '<span style="font-size: 26px;">'.'Jesteś odwiedzającym nr '.$counterVal .'</span>';
