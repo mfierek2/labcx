0f398f88de3c86aaf089a661feefacc1df562e44
Michał <mfierek2@users.noreply.github.com>
2016-05-19 23:48:58 +0200

    Update zad01.php

--- lab10/zad01.php
+++ lab10/zad01.php
@@ -1,23 +1,23 @@
 	<?php
-				if(!isset($_COOKIE['visits'])) {
-					echo '<span style="font-size: 26px;">'.'Jesteœ odwiedzaj¹cym nr '.$counterVal .'</span>';
-					setcookie('visits','diskfdsljfclksmi',time()+86400);
-					$counter_name = "counter.txt";
-					if (!file_exists($counter_name)) {
-						$f = fopen($counter_name, "w");
-						fwrite($f,"0");
-						fclose($f);
-					}
-
-					$f = fopen($counter_name,"r");
-					$counterVal = fread($f, filesize($counter_name));
-					fclose($f);
-					$counterVal++;
-					$f = fopen($counter_name, "w");
-					fwrite($f, $counterVal);
-					fclose($f); 
-				}
-				else
-					echo "Dziœ ju¿ tu by³eœ"
-
+		if(!isset($_COOKIE['visits'])) {
+			error_reporting(0);
+			$counter_name = "counter.txt";
+			if (!file_exists($counter_name)) {
+				$f = fopen($counter_name, "w");
+				fwrite($f,"0");
+				fclose($f);
+			}
+		$f = fopen($counter_name,"r");
+		$counterVal = fread($f, filesize($counter_name));
+		fclose($f);
+		$counterVal++;
+		$f = fopen($counter_name, "w");
+		fwrite($f, $counterVal);
+		fclose($f); 
+				
+		echo '<span style="font-size: 26px;">'.'JesteÅ› odwiedzajÄ…cym nr '.$counterVal .'</span>';
+		setcookie('visits','diskfdsljfclksmi',time()+86400);
+		}
+		else
+			echo "DziÅ› juÅ¼ tu byÅ‚eÅ›"
 	?>
